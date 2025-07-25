25bf8ce21de1f1c023d090cdab5c4b274af6d2c5
Freddiekinns <frederick.wildi@gmail.com>
2025-07-25 02:38:19 +0100

    refactor: Remove obsolete pages including DetailPage, EnhancedLanding, EnhancedOpeningDetail, HomePage, and OpeningDetailPageMinimal

--- packages/web/src/pages/HomePage.tsx
+++
@@ -1,8 +0,0 @@
-export default function HomePage() {
-  return (
-    <div className="home-page">
-      <h1>Chess Trainer</h1>
-      <p>Welcome to the Chess Opening Trainer</p>
-    </div>
-  )
-}

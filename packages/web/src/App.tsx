5277bc7404aaa361dd69ae086d6475b348b026e8
Freddiekinns <frederick.wildi@gmail.com>
2025-08-08 17:06:14 +0100

    feat: Add Vercel Analytics for improved tracking in the web application

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from 'react-router-dom'
+import { Analytics } from '@vercel/analytics/react'
 import LandingPage from './pages/LandingPage'
 import OpeningDetailPage from './pages/OpeningDetailPage'
 
@@ -10,6 +11,7 @@ function App() {
         <Route path="/opening/:fen" element={<OpeningDetailPage />} />
         <Route path="*" element={<LandingPage />} />
       </Routes>
+      <Analytics />
     </div>
   )
 }
